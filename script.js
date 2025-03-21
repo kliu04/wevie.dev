eb1367b54a4427f595fe976fe35855a605d59eaf
Kevin <kevinliu04@gmail.com>
2025-03-20 17:16:38 -0700

    add skeleton

---
+++ script.js
@@ -0,0 +1,4 @@
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('header').innerHTML = data)
+    .catch(error => console.error('Error loading header:', error));
