808bd201921772cc8ecf34ffeb96dd4d19bf5bfe
Kevin <kevinliu04@gmail.com>
2025-03-22 00:39:31 -0700

    feat: add footer

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ fetch('header.html')
     .then(response => response.text())
     .then(data => document.getElementById('header').innerHTML = data)
     .catch(error => console.error('Error loading header:', error));
+
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => document.getElementById('footer').innerHTML = data)
+    .catch(error => console.error('Error loading footer:', error));
